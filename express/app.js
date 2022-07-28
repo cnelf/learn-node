2995cc750a44db7ac89945f6bb11e26f56c707c6
cnelf <1184974829@qq.com>
2022-07-29 04:08:01 +0800

    feat: add static middleware

--- express/app.js
+++ express/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
@@ -8,6 +9,7 @@ const app = express();
 // Middleware
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, './public')));
 
 // Routes
 app.use('/api/v1/tours', tourRouter);
