e9956126810ec6309b5334ee1c519bdaf2201b08
cnelf <1184974829@qq.com>
2022-07-30 23:52:43 +0800

    feat: use mongoose connect mongodb

--- express/server.js
+++ express/server.js
@@ -2,7 +2,15 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
+const mongoose = require('mongoose');
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true
+  })
+  .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
